488669715c9dd75c049a4adc737cff4bf50655ba
Mohamed Abdo <el.king706@gmail.com>
2026-08-04 19:07:58 +0300

    Initial commit

---
+++ resources/views/job/index.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <h1>Job Board</h1>
+    <h3>My name is: {{$Name}}</h3>
+
+    @foreach ($jobs as $job)
+        <div>{{ $job['title'] }}:{{ $job['Salary']}}</div>
+    @endforeach
+</div>
